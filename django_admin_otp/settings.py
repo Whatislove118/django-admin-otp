2aa3a74e816c79e12eaa1738c08d31f434fefe43
whatislove118 <oleg.pochikalin@mail.ru>
2025-09-02 16:11:01 +0300

    fix: context processors

--- django_admin_otp/settings.py
+++ django_admin_otp/settings.py
@@ -10,7 +10,7 @@ PROJECT_NAME = os.environ.get("ADMIN_OTP_PROJECT_NAME", None)
 DEVICE_TOKEN_COOKIE_NAME = os.environ.get("ADMIN_OTP_DEVICE_TOKEN_COOKIE_NAME", "admin_otp_trusted_device")
 ADMIN_PATH = os.environ.get("ADMIN_PATH", "admin/")
 TRUSTED_DEVICE_DAYS = os.environ.get("ADMIN_OTP_TRUSTED_DEVICE_DAYS", 30)
-FORCE_OTP = bool(os.environ.get("ADMIN_OTP_FORCE", 0))
+FORCE_OTP = bool(int(os.environ.get("ADMIN_OTP_FORCE", 0)))
 
 
 def init():
