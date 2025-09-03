3eec1adc21b16f637501d9163bda8843fbf1cece
whatislove118 <oleg.pochikalin@mail.ru>
2025-09-03 13:21:28 +0300

    fix: mfa verify redirect

--- django_admin_otp/tests.py
+++ django_admin_otp/tests.py
@@ -240,7 +240,7 @@ class MFAVerifyViewTest(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, admin_url())
 
-    def test_get_request_go_to_admin_cause_no_verification_no_force_otp(self):
+    def test_get_request_go_to_admin_cause_no_verification_force_otp(self):
         old_value = settings.FORCE_OTP
         settings.FORCE_OTP = 1
         try:
@@ -254,6 +254,23 @@ class MFAVerifyViewTest(TestCase):
 
             self.assertEqual(response.status_code, 302)
             self.assertEqual(response.url, reverse(settings.MFA_SETUP_INTERNAL_NAME))
+        finally:
+            settings.FORCE_OTP = old_value
+
+    def test_get_request_go_to_admin_cause_no_verification_no_force_otp(self):
+        old_value = settings.FORCE_OTP
+        settings.FORCE_OTP = 0
+        try:
+            self.verification.delete()
+            request = self.factory.get("/mfa-verify/")
+            request.user = self.user
+            request.session = {}
+            request.COOKIES = {}
+
+            response = mfa_verify(request)
+
+            self.assertEqual(response.status_code, 302)
+            self.assertEqual(response.url, admin_url())
         finally:
             settings.FORCE_OTP = old_value
 
